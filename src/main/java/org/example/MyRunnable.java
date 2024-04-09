63adb0b93e83e8124ef7a102248bc5467beb18b5
Mikhail Vyskrebentsev <labola2016@yandex.ru>
2024-04-09 23:21:44 +0300

    Add Concurrency

---
+++ src/main/java/org/example/MyRunnable.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class MyRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.print("Run"+i+" | ");
+        }
+    }
+}
